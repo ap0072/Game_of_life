07bb1b961b84cf21b0d800fa060d1e7fc5c4fe5a
prasanth2020501099 <chaluvadikumar97@msitprogram.net>
2020-12-25 19:09:45 +0530

    javaFx

---
+++ ComplexNumber/ComplexNumber/src/socra.java
@@ -0,0 +1,21 @@
+//
+//class Child{
+//	Child(){
+//		x=x+10;
+//		System.out.println(x);
+//	}
+//}
+
+
+public class socra  {
+	static double price=0;
+	
+	
+	public static void main(String[] args) {
+	  changevalue();
+	  
+	}
+	static void changevalue() {
+		price=30;
+	}
+}
